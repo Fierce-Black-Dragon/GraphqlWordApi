0c50ddd44f1aa1ad94b550bab281e171f2e31420
Vishnu Kulkarni <blackdragon4204@gmail.com>
2021-11-05 00:08:04 +0530

    setup  mongodb and  made api call to oxford api

--- server.js
+++ server.js
@@ -2,45 +2,110 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { graphqlHTTP } from "express-graphql";
-
+import axios from "axios";
 import bodyParser from "body-parser";
-import {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLList,
-  GraphQLInt,
-  GraphQLNonNull,
-} from "graphql";
+import { graphql, buildSchema } from "graphql";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
+const Words = [];
+
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
 app.use(cors());
-app.listen(
-  PORT,
-  console.log(` server started and runing on http://localhost:${PORT}`)
-);
-const RootQueryType = new GraphQLObjectType({
-  name: "Query",
-  description: "Root Query",
-  fields: () => ({
-    hello: {
-      type: GraphQLString,
-      description: "Hello World",
-      resolve: () => "Hello World",
-    },
-  }),
-});
-const schema = new GraphQLSchema({
-  query: RootQueryType,
-  // mutation: RootMutationType
-});
+const dbURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqyqk.mongodb.net/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`;
+
+const db = mongoose
+  .connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(
+      PORT,
+      console.log(
+        ` server started and runing on http://localhost:${PORT}  database connceted :)`
+      )
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: schema,
+    schema: buildSchema(`
+        type Word{
+            _id:ID!
+            wordName:String!
+            definition:String!
+            examples:String
+            synonyms:[String]
+            Grammer:String
+        }
+        input WordInput{
+            wordName:String!
+         
+          
+        }
+
+          type RootQuery {
+              words: [Word!]!
+          }
+          type RootMutation {
+             createWord(wordInput:WordInput):Word
+          }
+          schema {
+              query: RootQuery
+              mutation: RootMutation
+          }
+      `),
+    rootValue: {
+      words: () => {
+        return Words;
+        // {
+
+        // },
+      },
+      createWord: async (args) => {
+        const apiresult = await axios.get(
+          `https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${args.wordInput.wordName}?strictMatch=true`,
+          {
+            headers: {
+              app_id: process.env.APP_ID,
+              app_key: process.env.APP_KEY,
+            },
+          }
+        );
+
+        const example =
+          apiresult.data?.results[0]?.lexicalEntries[0]?.entries[0]?.senses[0]
+            ?.examples[0].text;
+        const resultSynonyms =
+          apiresult.data?.results[0]?.lexicalEntries[0]?.entries[0]?.senses[1]?.synonyms?.map(
+            (p) => p.text
+          );
+
+        const word = {
+          _id: Math.random().toString(),
+          wordName: args.wordInput.wordName,
+          definition:
+            apiresult.data?.results[0]?.lexicalEntries[0]?.entries[0]?.senses[0]
+              ?.definitions[0],
+          examples: example,
+          Grammer:
+            apiresult.data?.results[0]?.lexicalEntries[0]?.lexicalCategory
+              ?.text,
+          synonyms: resultSynonyms,
+        };
+
+        Words.push(word);
+
+        return word;
+      },
+    },
     graphiql: true,
   })
 );
