96b4cd1272b422fea681fbefc166be6c46c20202
Vishnu Kulkarni <blackdragon4204@gmail.com>
2021-11-05 23:25:54 +0530

    clean up and  code structuring

--- graphQl/reslover/reslover.js
+++ graphQl/reslover/reslover.js
@@ -1,4 +1,5 @@
-import wordSchema from "./models/word.js";
+import wordSchema from "../../models/word.js";
+import axios from "axios";
 const rootReslover = {
   words: () => {
     return wordSchema
@@ -23,45 +24,53 @@ const rootReslover = {
       });
   },
   createWord: async (args) => {
-    const apiresult = await axios.get(
-      `https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${args.wordInput.wordName}?strictMatch=true`,
-      {
-        headers: {
-          app_id: process.env.API_ID,
-          app_key: process.env.API_KEY,
-        },
-      }
-    );
+    const foundWord = await wordSchema.findOne({
+      wordName: args.wordInput.wordName.toLowerCase(),
+    });
 
-    const example =
-      apiresult.data?.results[0]?.lexicalEntries[0]?.entries[0]?.senses[0]
-        ?.examples[0].text;
-    const resultSynonyms =
-      apiresult.data?.results[0]?.lexicalEntries[0]?.entries[0]?.senses[1]?.synonyms?.map(
-        (p) => p.text
+    if (foundWord) {
+      throw new Error("Word already exists");
+    } else {
+      const apiresult = await axios.get(
+        `https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${args.wordInput.wordName.toLowerCase()}?strictMatch=true`,
+        {
+          headers: {
+            app_id: process.env.API_ID,
+            app_key: process.env.API_KEY,
+          },
+        }
       );
 
-    const word = new wordSchema({
-      wordName: args.wordInput.wordName,
-      definition:
+      const example =
         apiresult.data?.results[0]?.lexicalEntries[0]?.entries[0]?.senses[0]
-          ?.definitions[0],
-      examples: example,
-      Grammer:
-        apiresult.data?.results[0]?.lexicalEntries[0]?.lexicalCategory?.text,
-      synonyms: resultSynonyms,
-    });
-    await word
-      .save()
-      .then((result) => {
-        console.log(result);
-        return { ...result._doc, _id: result._doc._id.toString() };
-      })
-      .catch((err) => {
-        console.log(err);
-        throw err;
+          ?.examples[0].text;
+      const resultSynonyms =
+        apiresult.data?.results[0]?.lexicalEntries[0]?.entries[0]?.senses[1]?.synonyms?.map(
+          (p) => p.text
+        );
+
+      const word = new wordSchema({
+        wordName: args.wordInput.wordName.toLowerCase(),
+        definition:
+          apiresult.data?.results[0]?.lexicalEntries[0]?.entries[0]?.senses[0]
+            ?.definitions[0],
+        examples: example,
+        Grammer:
+          apiresult.data?.results[0]?.lexicalEntries[0]?.lexicalCategory?.text,
+        synonyms: resultSynonyms,
       });
-    return word;
+      await word
+        .save()
+        .then((result) => {
+          console.log(result);
+          return { result };
+        })
+        .catch((err) => {
+          console.log(err);
+          throw err;
+        });
+      return word;
+    }
   },
 };
 export default rootReslover;
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { graphqlHTTP } from "express-graphql";
-import axios from "axios";
+
 import bodyParser from "body-parser";
-import { graphql, buildSchema } from "graphql";
+// import { graphql, buildSchema } from "graphql";
+
+import rootSchema from "./graphQl/schema/schema.js";
 import mongoose from "mongoose";
-import wordSchema from "./models/word.js";
+import rootReslover from "./graphQl/reslover/reslover.js";
 
 dotenv.config();
 const app = express();
@@ -36,99 +38,8 @@ const db = mongoose
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: buildSchema(`
-        type Word{
-            _id:ID!
-            wordName:String!
-            definition:String!
-            examples:String
-            synonyms:[String]
-            Grammer:String
-        }
-        input WordInput{
-            wordName:String!
-         
-          
-        }
-
-          type RootQuery {
-              words: [Word!]!
-              word(wordId:ID!): Word!
-          }
-          type RootMutation {
-             createWord(wordInput:WordInput):Word
-          }
-          schema {
-              query: RootQuery
-              mutation: RootMutation
-          }
-      `),
-    rootValue: {
-      words: () => {
-        return wordSchema
-          .find()
-          .then((words) => {
-            return words.map((word) => {
-              return { ...word._doc, _id: word.id };
-            });
-          })
-          .catch((err) => {
-            throw err;
-          });
-      },
-      word: (args) => {
-        return wordSchema
-          .findById(args.wordId)
-          .then((word) => {
-            return word;
-          })
-          .catch((err) => {
-            throw err;
-          });
-      },
-      createWord: async (args) => {
-        const apiresult = await axios.get(
-          `https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${args.wordInput.wordName}?strictMatch=true`,
-          {
-            headers: {
-              app_id: process.env.API_ID,
-              app_key: process.env.API_KEY,
-            },
-          }
-        );
-
-        const example =
-          apiresult.data?.results[0]?.lexicalEntries[0]?.entries[0]?.senses[0]
-            ?.examples[0].text;
-        const resultSynonyms =
-          apiresult.data?.results[0]?.lexicalEntries[0]?.entries[0]?.senses[1]?.synonyms?.map(
-            (p) => p.text
-          );
-
-        const word = new wordSchema({
-          wordName: args.wordInput.wordName,
-          definition:
-            apiresult.data?.results[0]?.lexicalEntries[0]?.entries[0]?.senses[0]
-              ?.definitions[0],
-          examples: example,
-          Grammer:
-            apiresult.data?.results[0]?.lexicalEntries[0]?.lexicalCategory
-              ?.text,
-          synonyms: resultSynonyms,
-        });
-        await word
-          .save()
-          .then((result) => {
-            console.log(result);
-            return { ...result._doc, _id: result._doc._id.toString() };
-          })
-          .catch((err) => {
-            console.log(err);
-            throw err;
-          });
-        return word;
-      },
-    },
+    schema: rootSchema,
+    rootValue: rootReslover,
     graphiql: true,
   })
 );
